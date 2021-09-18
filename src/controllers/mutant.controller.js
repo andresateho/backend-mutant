07ff492407dc9711c6eacb272ea5d938a9dff0dc
Andres Felipe Atehortua Londoño <afatehor@bancolombia.com.co>
2021-09-18 13:43:01 -0500

    Se crean metodos para validar y retornar info.

--- src/controllers/mutant.controller.js
+++ src/controllers/mutant.controller.js
@@ -1,3 +1,5 @@
+const wordSearchSolver = require('word-search-solver');
+const errorMiddleware = require('../middlewares/error.middleware');
 let _mutantService = null;
 
 class MutantController{
@@ -6,14 +8,44 @@ class MutantController{
     }
 
     async getAll(req, res){
-        const mutants = await _mutantService.getAll();
-        return res.send(mutants);
+        let count_mutant_dna_cont = 0;
+        let count_human_dna_cont = 0;
+
+        let mutants = await _mutantService.getAll();
+        mutants = mutants.map(result => {
+            if (result.isMutant)
+                count_mutant_dna_cont ++;          
+            else
+                count_human_dna_cont ++;
+        });
+    
+        return res.send(
+            { 
+                count_mutant_dna: count_mutant_dna_cont,
+                count_human_dna: count_human_dna_cont,
+                ratio: (count_mutant_dna_cont * 100) / (count_mutant_dna_cont + count_human_dna_cont)
+            }
+        );
     }
 
     async create(req, res){
         const { body } = req;
-        const mutants = await _mutantService.create(body);
-        return res.send(mutants);
+
+        const newArray = _mutantService.createArray(body);
+        const words = ['AAAA','TTTT', 'CCCC', 'GGGG'];
+        
+        const solution = await wordSearchSolver(newArray, words);
+
+        const isMutant = _mutantService.validateMutant(solution);
+        body.isMutant = isMutant;
+
+        await _mutantService.create(body);
+
+        if(isMutant){
+            return res.send();
+        }else{
+            return res.status(403).send();
+        }            
     }
 }
 
--- src/services/mutant.service.js
+++ src/services/mutant.service.js
@@ -6,6 +6,30 @@ class MutantServices extends BaseService{
         super(MutantRepository);
         _mutantRepository = MutantRepository;
     }
+
+    createArray (req){
+        let newArray = new Array(req.dna.length);
+
+        for(let i=0; i<req.dna.length; i++) {
+            let array = req.dna[i].split('');
+            newArray[i] = array;
+        }
+
+        return newArray;
+    }
+
+    validateMutant (req){
+        let num = 0;
+        let mutant = false;
+
+        req.map(valueTrue => valueTrue.found == true ? num++ : num);
+        
+        if(num > 1){
+            mutant = true;
+        }            
+
+        return mutant;
+    }
 }
 
 module.exports = MutantServices;
